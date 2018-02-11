41221f8bebdc9e748c7bdcda87a719d78ac814ec
RyanLaFary <30330802+RyanLaFary@users.noreply.github.com>
2018-02-11 16:37:37 -0500

    slider height and slider arrows

--- js/actions.js
+++ js/actions.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     $('body').toggleClass('open');
 	});
 
-  $('.your-class').slick({
-    nextArrow: '<button type="button" class="slick-next">></button>',
-    prevArrow: '<button type="button" class="slick-prev"><</button>'
+  $('.slideshow').slick({
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>'
   });
 });
