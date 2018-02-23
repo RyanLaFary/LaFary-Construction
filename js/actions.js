7bb20b96be42e53be7d0ab42cb7a49bbeb981596
RyanLaFary <30330802+RyanLaFary@users.noreply.github.com>
2018-02-22 21:32:30 -0500

    slideshow autoplay

--- js/actions.js
+++ js/actions.js
@@ -9,4 +9,12 @@ $(document).ready(function(){
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>'
   });
+
+	$('.autoplay').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 2000,
+});
+
 });
