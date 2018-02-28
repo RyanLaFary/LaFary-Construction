9ec3e3dca270dd7a39d892f50e5b3774770ab178
Dave Berning <daveberning@icloud.com>
2018-02-27 20:18:29 -0500

    slider autoplay

--- js/actions.js
+++ js/actions.js
@@ -5,16 +5,13 @@ $(document).ready(function(){
     $('body').toggleClass('open');
 	});
 
-  $('.slideshow').slick({
-    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
-    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>'
+	$('.slideshow').slick({
+  	  slidesToShow: 1,
+  	  slidesToScroll: 1,
+  	  autoplay: true,
+  	  autoplaySpeed: 10000,
+      nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+      prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>'
   });
-
-	$('.autoplay').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  autoplay: true,
-	  autoplaySpeed: 2000,
-});
 
 });
